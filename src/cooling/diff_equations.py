b855846167b63c3a0fd5ae9169b65706bd14d28c
Theo Wolf <theo@carbonre.com>
2023-03-20 09:30:57 +0000

    Update plots

--- src/cooling/diff_equations.py
+++ src/cooling/diff_equations.py
@@ -3,11 +3,11 @@ import numpy as np
 
 
 def grad(outputs, inputs):
-    """compute the derivative of outputs associated with inputs.
-    Params
-    ======
-    outputs: (N, 1) tensor
-    inputs: (N, D) tensor
+    """Computes the partial derivative of 
+    an output with respect to an input.
+    Args:
+        outputs: (N, 1) tensor
+        inputs: (N, D) tensor
     """
     return torch.autograd.grad(
         outputs, inputs, grad_outputs=torch.ones_like(outputs), create_graph=True
